d65a83f1762d3d63e2b7c582143333c5f1a1a05c
arghjain29 <arghjain1234@gmail.com>
2024-07-15 18:06:48 +0530

    button modified

--- app/src/main/java/com/example/maincalculator/MainActivity.kt
+++ app/src/main/java/com/example/maincalculator/MainActivity.kt
@@ -1,10 +1,17 @@
 package com.example.maincalculator
 
+import android.animation.ArgbEvaluator
+import android.animation.ValueAnimator
+import android.graphics.drawable.GradientDrawable
 import android.os.Bundle
+import android.view.animation.AccelerateDecelerateInterpolator
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
 import android.widget.Button
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import net.objecthunter.exp4j.ExpressionBuilder
@@ -39,6 +46,8 @@ class MainActivity : AppCompatActivity() {
             insets
         }
 
+        val clickAnimation: Animation = AnimationUtils.loadAnimation(this, R.anim.button_click)
+
         numView = findViewById(R.id.numView)
         resultView = findViewById(R.id.resultView)
 
@@ -66,32 +75,39 @@ class MainActivity : AppCompatActivity() {
 
 
 
-        // Setting up button listeners
-        button0.setOnClickListener { appendOnExpression("0", true) }
-        button1.setOnClickListener { appendOnExpression("1", true) }
-        button2.setOnClickListener { appendOnExpression("2", true) }
-        button3.setOnClickListener { appendOnExpression("3", true) }
-        button4.setOnClickListener { appendOnExpression("4", true) }
-        button5.setOnClickListener { appendOnExpression("5", true) }
-        button6.setOnClickListener { appendOnExpression("6", true) }
-        button7.setOnClickListener { appendOnExpression("7", true) }
-        button8.setOnClickListener { appendOnExpression("8", true) }
-        button9.setOnClickListener { appendOnExpression("9", true) }
-        buttonDot.setOnClickListener { appendOnExpression(".", true) }
+        //Setting up button listeners
+        button0.setOnClickListener {it.startAnimation(clickAnimation); animateButtonColor(button0); appendOnExpression("0", true) }
+        button1.setOnClickListener {it.startAnimation(clickAnimation); animateButtonColor(button1); appendOnExpression("1", true) }
+        button2.setOnClickListener {it.startAnimation(clickAnimation); animateButtonColor(button2); appendOnExpression("2", true) }
+        button3.setOnClickListener {it.startAnimation(clickAnimation); animateButtonColor(button3); appendOnExpression("3", true) }
+        button4.setOnClickListener {it.startAnimation(clickAnimation); animateButtonColor(button4); appendOnExpression("4", true) }
+        button5.setOnClickListener {it.startAnimation(clickAnimation); animateButtonColor(button5); appendOnExpression("5", true) }
+        button6.setOnClickListener {it.startAnimation(clickAnimation); animateButtonColor(button6); appendOnExpression("6", true) }
+        button7.setOnClickListener {it.startAnimation(clickAnimation); animateButtonColor(button7); appendOnExpression("7", true) }
+        button8.setOnClickListener {it.startAnimation(clickAnimation); animateButtonColor(button8); appendOnExpression("8", true) }
+        button9.setOnClickListener {it.startAnimation(clickAnimation); animateButtonColor(button9); appendOnExpression("9", true) }
+        buttonDot.setOnClickListener {it.startAnimation(clickAnimation); animateButtonColor(buttonDot); appendOnExpression(".", true) }
 
-        buttonAdd.setOnClickListener { appendOnExpression("+", false) }
-        buttonSub.setOnClickListener { appendOnExpression("-", false) }
-        buttonMul.setOnClickListener { appendOnExpression("*", false) }
-        buttonDiv.setOnClickListener { appendOnExpression("/", false) }
+        buttonAdd.setOnClickListener {it.startAnimation(clickAnimation); animateButtonColor(buttonAdd); appendOnExpression("+", false) }
+        buttonSub.setOnClickListener {it.startAnimation(clickAnimation); animateButtonColor(buttonSub); appendOnExpression("-", false) }
+        buttonMul.setOnClickListener {it.startAnimation(clickAnimation); animateButtonColor(buttonMul); appendOnExpression("*", false) }
+        buttonDiv.setOnClickListener {it.startAnimation(clickAnimation); animateButtonColor(buttonDiv); appendOnExpression("/", false) }
+
+
 
         buttonResult.setOnClickListener {
             try {
+
                 val expression = ExpressionBuilder(numView.text.toString()).build()
                 val result = expression.evaluate()
                 val longResult = result.toLong()
                 if (result == longResult.toDouble()) {
+                    it.startAnimation(clickAnimation);
+                    animateButtonColorEqual(buttonResult);
                     resultView.text = longResult.toString()
                 } else {
+                    it.startAnimation(clickAnimation);
+                    animateButtonColorEqual(buttonResult);
                     resultView.text = result.toString()
                 }
             } catch (e: Exception) {
@@ -100,16 +116,53 @@ class MainActivity : AppCompatActivity() {
         }
 
         buttonAC.setOnClickListener {
+            it.startAnimation(clickAnimation);
+            animateButtonColor(buttonAC);
             numView.text = ""
             resultView.text = ""
         }
 
         buttonBack.setOnClickListener {
+            it.startAnimation(clickAnimation);
+            animateButtonColor(buttonBack);
             val string = numView.text.toString()
             if (string.isNotEmpty()) {
                 numView.text = string.substring(0, string.length - 1)
             }
             resultView.text = ""
         }
+
+
     }
+
+    private fun animateButtonColor(button: Button) {
+        val colorFrom = ContextCompat.getColor(this, R.color.original_button_color)
+        val colorTo = ContextCompat.getColor(this, R.color.lighter_button_color)
+        val background = button.background as GradientDrawable
+
+            val colorAnimation = ValueAnimator.ofObject(ArgbEvaluator(), colorFrom, colorTo, colorFrom).apply {
+                duration = 600 // Total duration of the color animation
+                interpolator = AccelerateDecelerateInterpolator() // Smooth fade in and out
+                addUpdateListener { animator ->
+                    background.setColor(animator.animatedValue as Int)
+                }
+            }
+        colorAnimation.start()
+    }
+
+    private fun animateButtonColorEqual(button: Button) {
+        val colorFrom = ContextCompat.getColor(this, R.color.lighter_button_color)
+        val colorTo = ContextCompat.getColor(this, R.color.original_button_color)
+        val background = button.background as GradientDrawable
+
+        val colorAnimation = ValueAnimator.ofObject(ArgbEvaluator(), colorFrom, colorTo, colorFrom).apply {
+            duration = 600 // Total duration of the color animation
+            interpolator = AccelerateDecelerateInterpolator() // Smooth fade in and out
+            addUpdateListener { animator ->
+                background.setColor(animator.animatedValue as Int)
+            }
+        }
+        colorAnimation.start()
+    }
+
 }
